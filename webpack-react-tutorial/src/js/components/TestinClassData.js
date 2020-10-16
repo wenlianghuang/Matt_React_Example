ab3aea5ef884b464b5a13979038d5925835d5ec5
wenlianghuang <wen-liang.huang@cern.ch>
2020-10-16 13:37:44 +0800

    20201016 new file of webpack-react-tutorial/src/

---
+++ webpack-react-tutorial/src/js/components/TestinClassData.js
@@ -0,0 +1,16 @@
+const columnIndex = ['Subject','Scores'];
+const scoreCard = {
+    name: '',
+    records: [
+        /*
+        ['Math', 100 ],
+        [ 'Chinese', 87 ],
+        [ 'English', 100 ],
+        [ 'Science', 100 ],
+        [ 'Social', 90 ],
+        [ 'History', 60]
+        */
+    ]
+};
+
+export {columnIndex,scoreCard};
