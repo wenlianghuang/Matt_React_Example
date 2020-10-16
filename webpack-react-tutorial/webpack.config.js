7dc7cb1f9bfca74c3a7b5a6f8377f149f3dbb2e8
wenlianghuang <wen-liang.huang@cern.ch>
2020-10-16 13:38:24 +0800

    20201016 new file of webpack-react-tutorial/webpack.config.js

---
+++ webpack-react-tutorial/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+ 
+module.exports = {
+  entry: ['./src/index.js'],
+  
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+          test: /\.html$/,
+          use: [
+              {
+                  loader: "html-loader"
+              }
+          ]
+      },
+      {
+        test: /\.css$/, // 正則式，$ 用以表示以 .css 結尾的檔案，反斜線是跳脫字元
+        use: ["style-loader", "css-loader"]
+      }
+      
+    ]
+  },
+  plugins:[
+    new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
