67b6a094d7cbccbc175dffed8c86bd6ab329a187
wenlianghuang <wen-liang.huang@cern.ch>
2020-10-18 00:15:16 +0800

    20201017 modified file in src/ folder

--- webpack-react-tutorial/src/index.js
+++ webpack-react-tutorial/src/index.js
@@ -1,7 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Form from "./js/components/Form";
 import TestforMe from "./js/components/TestinClass";
 import {columnIndex,scoreCard} from "./js/components/TestinClassData";
+import Example from "./js/components/AlertDismissibleExample"
 ReactDOM.render(<Form scoreCard={scoreCard} columnIndex={columnIndex}/>,document.getElementById('container'));
+ReactDOM.render(<Example/>,document.getElementById("Test"));
 //ReactDOM.render(<TestforMe scoreCard = {scoreCard} columnIndex = {columnIndex} />,document.getElementById("ExampleinClass"))
