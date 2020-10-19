0e375392128d168e8eb774a608a0088487e6af30
wenlianghuang <wen-liang.huang@cern.ch>
2020-10-19 10:35:39 +0800

    20201019 new file of Array Data

---
+++ webpack-react-tutorial/src/js/components/CarDetail.js
@@ -0,0 +1,25 @@
+const CarDetail = {
+    
+    Audi: [
+        ['Manufacture',2003],
+        ['Litter',2000],
+        ['MadeInLocation','Taiwan']
+    ],
+    BMW: [
+        ['Manufacture',2019],
+        ['Litter',2500],
+        ['MadInLocation','Chinese']
+    ],
+    Mercedes: [
+        ['Manufacture',1995],
+        ['Litter',1900],
+        ['MadInLocation','Germen']
+    ],
+    Volvo: [
+        ['Manufacuture',2008],
+        ['Litter',2300],
+        ['MadInLocation','Thailand']
+    ]
+}
+
+export {CarDetail};
